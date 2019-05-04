f2ffdd73484aac06f86c7d09ecb7bf75de65061a
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2019-05-04 20:42:00 +0400

    feature extraction. save features to files

--- source/dnn/dnn_main.py
+++ source/dnn/dnn_main.py
@@ -3,18 +3,31 @@ sys.path.append('.')
 
 from PyQt5 import QtWidgets, QtGui, QtCore
 from view.teacher_view import Ui_MainWindow
+from feature_extractor import *
 
 class mainWindow(QtWidgets.QMainWindow):
+    dname = ""
     def __init__(self):
         super(mainWindow,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.ui.pb_path.clicked.connect(self.pathSelect)
+        self.ui.pb_exctract.clicked.connect(self.extractFeatures)
     
     def pathSelect(self):
-        dname = QtWidgets.QFileDialog.getExistingDirectory(self,"Select Directory")
-        self.ui.le_path.setText(dname)
+        self.dname = QtWidgets.QFileDialog.getExistingDirectory(self,"Select Directory")
+        self.ui.le_path.setText(self.dname)
+
+    def extractFeatures(self):
+        QtCore.qDebug("Extract features from ")
+        QtCore.qDebug(self.dname)
+        sub_dirs = os.listdir(self.dname)
+        features, labels = parse_files(self.dname, sub_dirs)
+        np.save("models/X", features)
+        labels = one_hot_encode(labels)
+        np.save("models/Y", labels)
+        QtCore.qDebug("Features were extracted")
 
 app = QtWidgets.QApplication([])
 win = mainWindow()
